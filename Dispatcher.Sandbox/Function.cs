f4a243fcdd41b030adc76a509648a4c8ba29eed7
Alan Wills <alawills@googlemail.com>
2018-07-29 17:16:53 +0100

    Added sandbox skill

---
+++ Dispatcher.Sandbox/Function.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Response;
+using RealTalkEngine.RequestHandling.RequestHandlers;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace Dispatcher.Sandbox
+{
+    public class Function
+    {
+        /// <summary>
+        /// All of the available skill request handlers that can handle incoming skill requests.
+        /// </summary>
+        private static List<SkillRequestHandler> SkillRequestHandlers { get; set; } = new List<SkillRequestHandler>()
+        {
+            // Intent request is most likely
+            new IntentRequestHandler(),
+            new LaunchRequestHandler(),
+            new SessionEndedRequestHandler()
+        };
+
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            context.Logger.LogLine("Request Type: " + input.GetRequestType().Name);
+
+            // Either use the appropriate request handler or just return an empty response.
+            SkillRequestHandler appropriateRequestHandler = SkillRequestHandlers.Find(x => x.IsHandlerForRequest(input));
+            return appropriateRequestHandler != null ? appropriateRequestHandler.HandleRequest() : ResponseBuilder.Empty();
+        }
+    }
+}
